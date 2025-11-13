5d1a5c8f7c95ce5ea2831e6d7f5f8f57b38044b7
siddarthadbt@gmail.com <siddarthadbt@gmail.com>
2025-11-13 08:45:33 +0000

    ,,

--- models/Staging/customers.sql
+++ models/Staging/customers.sql
@@ -3,19 +3,3 @@ select
     first_name,
     last_name
 from raw_demo.jaffle_shop.customers
-
-/*
-select
-    id as customer_id,
-    first_name,
-    last_name
-from {{ source('jaffle_shop1', 'stg_customers') }}
-
-UNION ALL
-
-select
-    ID,
-    FIRST_NAME,
-    LAST_NAME
-FROM {{ ref('TEST_CUST') }}
-*/
--- models/Staging/orders.sql
+++ models/Staging/orders.sql
@@ -4,23 +4,3 @@ select
     order_date,
     status
 from raw_demo.jaffle_shop.orders
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-select
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status
-from {{ source('jaffle_shop1', 'stg_orders') }}
-*/
